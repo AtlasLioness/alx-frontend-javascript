1c2ad9c7500c6cbae9dc5c68d5af691026502ce6
AtlasLioness <narjisaliane@gmail.com>
2024-11-13 20:24:48 +0000

    6. Handle multiple promises - fixed

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,9 +4,14 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promise1 = signUpUser(firstName, lastName);
   const promise2 = uploadPhoto(fileName);
-  return Promise.allSettled([promise1, promise2])
-    .then((results) => results.map((result) => ([{
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    }])));
+  const result = [];
+  return Promise.allSettled([promise1, promise2]).then((results) => {
+    results.map(({ status, value, reason }) => (
+      result.push({
+        status,
+        value: status === 'fulfilled' ? value : reason.toString(),
+      })
+    ));
+    return result;
+  });
 }
